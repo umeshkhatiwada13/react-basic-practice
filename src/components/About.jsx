f02fb6cd370fd1746671f7a27466ba8e751a61cf
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-16 10:35:37 +0545

    exchang home and about component contents

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,47 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useState } from 'react';
-import Coin from './Coin';
-import Loader from './Loader';
+import React, { useState, useEffect } from 'react';
+import { ProductMap } from './Product';
 
 const About = () => {
-  const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(true);
 
+
+
+  const dataArray = [1, 2, 3, 4];
+  //defining useState hook
+  // temp is variable name and setTemp is setter used to populate the variable later on program flow
+  // 0 is passed in the useState to set initial value of temp 0
+  // setTemp is called on button press later on program updating the value of temp variable
+  const [temp, setTemp] = useState(0);
+
+  // this hook is called everytime the method is mounted / updated
   useEffect(() => {
-    const fetchAllCoins = async () => {
-      try {
-        const { data } = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20");
-        setCoins(data);
-      } catch (e) {
-        console.log("Error : " + e);
-      }
-      setLoading(false);
+    //after mounting
+    console.log("Home Mounting");
+    //return is called at unmounting
+    return () => {
+      console.log("Home Unmounting");
     }
-
-    fetchAllCoins();
-  }, [])
-
+  },//Empty array below is used to pass dependency or the states which we have to keep track of
+    // if used useEffect is called only upon object rendering ie mounting
+    // else it is called on everytime a variable is updated 
+    [])
   return (
-    <div className='about'>
-      {
-        loading ? <Loader /> :
-          coins.map((i) => (
-            <Coin name={i.name} symbol={i.symbol} key={i.key} imgSrc={i.image} price={i.current_price} />
+    <>
+      <div>
+        <button onClick={() => setTemp(temp + 1)}>Press me</button>
+        <h2>Temp Counter</h2>
+        <h3>{temp}</h3>
+        {
+          dataArray.map((i) => (
+            <>
+              <ProductMap value={i} key={i} />
+            </>
           ))
-      }
-    </div>
+        }
+      </div>
+      <div>
+
+      </div>
+    </>
   )
 }
 
